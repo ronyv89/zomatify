ee3cf810bcd7300a2f544adfd786c2fa58231619
Rony Varghese <ronyv250289@gmail.com>
2016-02-19 09:06:08 +0530

    Setup simplecov-rcov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,25 @@ require 'spork'
 #require 'spork/ext/ruby-debug'
 
 Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
+  unless ENV['DRB']
+    require 'simplecov'
+    require 'simplecov-rcov'
+    SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+    SimpleCov.start
+  end
 
+  # other code ...
 end
 
 Spork.each_run do
-  # This code will be run each time you run your specs.
+  if ENV['DRB']
+    require 'simplecov'
+    require 'simplecov-rcov'
+    SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+    SimpleCov.start
+  end
 
+  # other code ...
 end
 
 # --- Instructions ---
